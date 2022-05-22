9eca2eba35b39420c223b261d7c2bee484ba1eca
손영주 <@DESKTOP-PGK68U5>
2022-05-22 20:24:03 +0900

    멘토 응용문제(3문제)

---
+++ java_notion_11th_apply/src/prac1/Main.java
@@ -0,0 +1,78 @@
+package prac1;
+import java.util.Scanner;
+
+class Mart{
+	int purchase;  //구매금액
+	String membername;  //회원아이디
+	boolean member=false;  //회원여부
+	
+	public Mart(int purchase) {
+		this.purchase=purchase;
+	}
+	
+	public Mart(String membername, int purchase) {
+		this.membername = membername;
+		this.member = true;
+		this.purchase=purchase;
+	}
+	
+	public void Sale() {
+		/*구매금액이 백만원일 경우거나 음수를 입력할 경우*/
+		if(this.purchase>1000000 || this.purchase<0) {
+			System.out.println("올바르지 않습니다.");
+		}
+		
+		/*회원일 경우*/
+		else if(member) {
+			/*10,000원 이상일 경우 : 15% 할인*/
+			if(this.purchase>=10000 && this.purchase <50000) {
+				this.purchase = (int)(this.purchase*0.85); 
+			}
+			/*50,000원 이상일 경우 : 20% 할인*/
+			else if(this.purchase>=50000 && this.purchase < 100000) {
+				this.purchase = (int)(this.purchase*0.8);
+			}
+			/*100,000원 이상일 경우 : 30% 할인*/
+			else {
+				this.purchase = (int)(this.purchase*0.7);
+			}
+			System.out.println("회원 ID : " + this.membername + "의 최종 결제액은 " + this.purchase + "입니다.");
+		}
+		
+		/*비회원일 경우*/
+		else {
+			/*10,000원 이상일 경우 : 5% 할인*/
+			if(this.purchase>=10000 && this.purchase <50000) {
+				this.purchase = (int)(this.purchase*0.95);
+			}
+			/*50,000원 이상일 경우 : 10% 할인*/
+			else if(this.purchase>=50000 && this.purchase < 100000) {
+				this.purchase = (int)(this.purchase*0.9);
+			}
+			/*100,000원 이상일 경우 : 20% 할인*/
+			else {
+				this.purchase = (int)(this.purchase*0.8);
+			}
+			System.out.println("최종 결제액은 " + this.purchase + "입니다.");
+		}		
+	}	
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("구매할 금액을 입력하세요 : ");
+		int a_purchase = sc.nextInt();
+		/*비회원 객체 생성 및 Sale() 메소드 실행*/
+		Mart a = new Mart(a_purchase);
+		a.Sale();
+		
+		System.out.print("회원번호와 금액을 입력하세요 : ");
+		String member = sc.next();
+		int b_purchase = sc.nextInt();
+		/*회원 객체 생성 및 Sale() 메소드 실행*/
+		Mart b = new Mart(member,b_purchase);
+		b.Sale();
+	}
+
+}
---
+++ java_notion_11th_apply/src/prac2/Main.java
@@ -0,0 +1,70 @@
+package prac2;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Shape{
+	int a,b,c;
+	
+	public Shape(int a) {
+		this.a=a;		
+		System.out.println("직선의 길이는 : " + this.a);
+	}
+	
+	public Shape(int a,int b) {
+		this.a=a;
+		this.b=b;		
+		System.out.println("직사각형의 넓이는 : "+(this.a)*(this.b));
+	}
+	
+	public Shape(int a, int b, int c) {
+		this.a=a;
+		this.b=b;
+		this.c=c;	
+		System.out.println("직육면체 넓이는 : "+(this.a)*(this.b)*(this.c));
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("수치를 입력하시오 (1개 : 직선, 2개 : 직사각형, 3개 : 직육면체)");
+		String input_line = sc.nextLine();
+		
+		int a=0,b=0,c=0;   
+		
+		String []arr = input_line.split(" ");
+		/*입력받은 수 구분짓기*/
+		switch(arr.length) {
+		/*직선일 경우*/
+		case 1:
+			a=Integer.parseInt(arr[0]);
+			break;
+		/*직사각형일 경우*/
+		case 2:
+			a=Integer.parseInt(arr[0]);
+			b=Integer.parseInt(arr[1]);
+			break;
+		/*직육면체일 경우*/
+		case 3:
+			a=Integer.parseInt(arr[0]);
+			b=Integer.parseInt(arr[1]);
+			c=Integer.parseInt(arr[2]);
+			break;
+		}
+		
+		/*직선일 경우*/
+		if(a!=0 && b==0 && c==0) {
+			Shape shape_line = new Shape(a);
+		}
+		/*직사각형일 경우*/
+		else if(a!=0 && b!=0 && c==0) {
+			Shape shape_rectangle = new Shape(a,b);
+		}
+		/*직육면체일 경우*/
+		else if(a!=0 && b!=0 && c!=0) {
+			Shape shape_cuboid = new Shape(a,b,c);
+		}
+		
+	}
+}
